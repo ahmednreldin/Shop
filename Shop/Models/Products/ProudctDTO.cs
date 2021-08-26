c2fda5b0753f7e2117f34ccf434fb9b63ea74cc3
ahmednreldin <m-32500912@s-mu.edu.eg>
2021-08-26 16:43:03 +0200

    DTO : Create Product

---
+++ Shop/Models/Products/ProudctDTO.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Shop.Web.Models.Products
+{
+    public class ProudctDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Salery { get; set; }
+        public IFormFile ImageFile { get; set; }
+    }
+}
