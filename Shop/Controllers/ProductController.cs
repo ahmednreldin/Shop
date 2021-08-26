d91b09f8c8a90d3396a7c170d5ac51de6ed64a84
ahmednreldin <m-32500912@s-mu.edu.eg>
2021-08-26 17:34:03 +0200

    Controller : Create Product

---
+++ Shop/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Models.Products;
+using Shop.Web.Models.Products;
+using Shop.Web.Services.Fondations.FileManager;
+using Shop.Web.Services.Fondations.Products;
+
+namespace Shop.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        IFileManager fileManager;
+        IProductService productService;
+
+        public ProductController(IFileManager fileManager, IProductService productService)
+        {
+            this.fileManager = fileManager;
+            this.productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+            [HttpPost]
+        public IActionResult Create(ProductDTO productDTO)
+        {
+            try
+            {
+                Product product = MapProductDTO(productDTO);
+                productService.AddProductAsync(product);
+            }
+            catch { }
+            return View(productDTO);
+        }
+
+        private Product MapProductDTO(ProductDTO productDTO)
+        {
+            return new Product
+            {
+                Name = productDTO.Name,
+                Salery = productDTO.Salery,
+                Description = productDTO.Description,
+                ImageUrl = fileManager.SaveImage(productDTO.ImageFile)
+            };
+        }
+    }
+}
