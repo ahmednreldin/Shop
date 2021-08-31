1dc2fe0ff76a0943f260a757c1c14eba9246f200
ahmednreldin <m-32500912@s-mu.edu.eg>
2021-08-31 21:25:29 +0200

    Model : Product add properties

--- src/Domain/Models/Products/Product.cs
+++ src/Domain/Models/Products/Product.cs
@@ -4,10 +4,15 @@ namespace Domain.Models.Products
 {
     public class Product
     {
-        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
-        public double Salery { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string Picture { get; set; }
+        public double Price { get; set; }
+        public int StockQuantity { get; set; }
+        public bool ShowOnHomepage { get; set; }
+        public string Sku { get; set; }
+
     }
 }
