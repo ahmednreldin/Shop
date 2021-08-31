82a2205f7537231f5c641d1c39c26cc43bef5d7f
ahmednreldin <m-32500912@s-mu.edu.eg>
2021-08-31 21:25:06 +0200

    Logging : configuration

---
+++ src/Application/Loggins/ILogging.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Application.Loggins
+{
+    public interface ILogging
+    {
+        void LogInformation(string message);
+        void LogDebug(string message);
+        void LogWarning(string message);
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
+    }
+}
---
+++ src/Infrastructure/Loggins/Logging.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Application.Loggins;
+
+namespace Infrastructure.Loggins
+{
+    public class Logging : ILogging
+    {
+        private readonly ILogger<Logging> logger;
+
+        public Logging(ILogger<Logging> logger)
+        {
+            this.logger = logger;
+        }
+
+        public void LogCritical(Exception exception) => 
+            this.logger.LogCritical(exception,exception.Message);
+
+        public void LogDebug(string message) =>
+            this.logger.LogDebug(message);
+
+        public void LogError(Exception exception) =>
+            this.logger.LogError(exception,exception.Message);
+
+        public void LogInformation(string message) =>
+            this.logger.LogInformation(message);
+
+        public void LogWarning(string message) =>
+            this.logger.LogWarning(message);
+    }
+}
